96815bc3628b67e34e8c44c5b38eb6922aa68f86
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 23:44:51 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,12 @@ class UsersController < ApplicationController
 
   def create
     binding.pry
-    user = User.create(name: params[:user][:name], password: params[:user][:password])
-    session[:user_id] = user.id
-    redirect_to "/users/home"
-
-
-  end
+    if params[:user][:password] == params[:user][:password_confirmation] 
+      user = User.create(name: params[:user][:name], password: params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to "/users/home"
+    else
+      render :new 
+    end 
+  end  
 end
