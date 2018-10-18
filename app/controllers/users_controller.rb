3f0ed1091dcbc2bfca9d3602c53798eede4c257b
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 23:47:32 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     if params[:user][:password] == params[:user][:password_confirmation]
       user = User.create(name: params[:user][:name], password: params[:user][:password])
       session[:user_id] = user.id
-      redirect_to :home
+      redirect_to '/home'
     else
       render :new
     end
