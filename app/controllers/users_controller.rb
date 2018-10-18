7fd27a32d3f43aeebbadf82342793a577eab03af
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 23:38:22 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   end
 
   def create
-
-    @user = User.new(name: params[:user][:name], password: params[:user][:password])
+    
+    @user = User.create(name: params[:user][:name], password: params[:user][:password])
     redirect_to "/users/home"
 
 
