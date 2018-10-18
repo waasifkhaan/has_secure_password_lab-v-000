defb935feb34c96c24ef0ef786983678e49cb661
Wasif Khan <waasif.khaan@gmail.com>
2018-10-18 23:46:08 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to "/users/home"
     else
-      render :new
+      redirect_to "/users/new"
     end
   end
 end
